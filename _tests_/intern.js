6cebe50c6806ae5e615939d814ee01501f9eba98
Rebecca Lacer <r.a.lacer@hotmail.com>
2021-04-19 14:46:24 -0600

    setting up

---
+++ _tests_/intern.js
@@ -0,0 +1,9 @@
+const Employee = require('./employee');
+
+class Intern extends Employee {
+  constructor(name, id, email, role) {
+    super(name, id, email);
+    this.school = school;
+    const role = new role("Intern");
+   
+  }}
