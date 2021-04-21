bbbb1f91d0741b953d4602ad8bf8d94c3e596018
Rebecca Lacer <r.a.lacer@hotmail.com>
2021-04-20 21:50:29 -0600

    more file setup

---
+++ src/template.js
@@ -0,0 +1,13 @@
+// help us create the team by running some functions that output a string this is HTML
+// const generateTeam = => {
+//     return
+// }
+
+const generateManager = manager => {
+    return`<div>HTML ${manager.getName()}</div>`}
+
+
+
+module.exports = team => {
+    return
+}
